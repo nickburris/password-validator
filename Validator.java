2969606b120bcb81f3ab4c048364df17c7dbddbe
Nick Burris <nickburris13@gmail.com>
2018-02-09 17:12:24 -0400

    Add 4 more validator rules for stage 2

--- Validator.java
+++ Validator.java
@@ -14,6 +14,36 @@ public class Validator{
 			passes++;
 		}
 		
+		// Check if there are upper and lower case letters
+		if(!password.toLowerCase().equals(password) && !password.toUpperCase().equals(password)){
+			passes++;
+		}
+		
+		// Check if the password contains a digit
+		for(char c : password.toCharArray()){
+			if(c >= '0' && c <= '9'){
+				passes++;
+				break;
+			}
+		}
+		
+		// Check if the password contains a special characters
+		for(char c : password.toCharArray()){
+			if(c < '0' || (c > '9' && c < 'A') || (c > 'Z' && c < 'a') || c > 'z'){
+				passes++;
+				break;
+			}
+		}
+		
+		// Make sure password is not all the same character
+		char f = password.charAt(0);
+		for(char c : password.toCharArray()){
+			if(f != c){
+				passes++;
+				break;
+			}
+		}
+		
 		return passes;
 	}
 }
