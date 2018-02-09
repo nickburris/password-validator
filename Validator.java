8944dd9f6e3f3c469fa685b977f01ba2d6e2b319
Nick Burris <nickburris13@gmail.com>
2018-02-09 17:03:03 -0400

    Finish up stage 1 validator and tests

--- Validator.java
+++ Validator.java
@@ -1,13 +1,19 @@
 public class Validator{
+	public static int NUM_TESTS = 2;
+	
 	public int validate(String password){
 		int passes = 0;
 		
-		if(!password.equals("password")){
+		// Check password is not equal to "password"
+		if(!password.toLowerCase().equals("password")){
 			passes++;
 		}
 		
+		// Check password is at least 8 characters long
 		if(password.length() >= 8){
 			passes++;
 		}
+		
+		return passes;
 	}
 }
