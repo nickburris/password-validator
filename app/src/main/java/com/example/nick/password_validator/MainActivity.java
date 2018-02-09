99395bd31c962755c6af2efd0c69fa980b40ecf0
Nick Burris <nickburris13@gmail.com>
2018-02-09 18:48:19 -0400

    Add validator unit tests and UI

--- app/src/main/java/com/example/nick/password_validator/MainActivity.java
+++ app/src/main/java/com/example/nick/password_validator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.nick.password_validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void validatePassword(View v){
+        String password = ((EditText)findViewById(R.id.editText)).getText().toString();
+        Validator val = new Validator();
+        int result = val.validate(password);
+        String quality = "Not strong";
+        if(result == Validator.NUM_TESTS){
+            quality = "Strongest";
+        }else if(result >= Validator.NUM_TESTS/2){
+            quality = "Somewhat strong";
+        }
+
+        ((TextView)findViewById(R.id.textView)).setText(quality);
     }
 }
